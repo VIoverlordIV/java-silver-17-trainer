c1eb77d53e2fab615f89866076c414c942efbcfe
TyouEi <qq1295490152@gmail.com>
2026-07-09 15:46:04 +0900

    Add stage model

---
+++ app/src/main/java/com/vioverlord/java17trainer/domain/Stage.java
@@ -0,0 +1,2 @@
+package com.vioverlord.java17trainer.domain;
+public final class Stage { public final String id; public final Text title; public final Text description; public Stage(String id, Text title, Text description){this.id=id;this.title=title;this.description=description;} }
